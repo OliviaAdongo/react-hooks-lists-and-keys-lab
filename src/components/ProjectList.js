54a80ccfbe409a0fc5a2bf3b62be1c0e208039a5
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-18 01:19:40 +0300

    Done.

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import user from "../data/user";
 
 function ProjectList({ projects }) {
+  const projectItems = projects.map((project)=>(
+    <ProjectItem
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+    />
+  ));
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+          {projectItems}
+        {/* render ProjectItem components here */}</div>
     </div>
   );
 }
